ba1dad9408444817881f540a799fe7a7f07ec459
mani vincent <manisebastine@yahoo.com>
2021-10-26 23:47:00 +0400

    initial

---
+++ emailer/src/main/java/com/jklm/pullingo/mail/EmailStuff.java
@@ -0,0 +1,353 @@
+package com.jklm.pullingo.mail;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import java.io.*;
+import javax.activation.*;
+import javax.mail.Session;
+import javax.mail.internet.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+ 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+ 
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import com.itextpdf.text.Anchor;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.List;
+import com.itextpdf.text.ListItem;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class EmailStuff {
+
+	public static void main(String[] args) {
+
+        // Recipient's email ID needs to be mentioned.
+        String to = "jlmvdxb@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "jlmvdxb@gmail.com";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("*********", "********");
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            //MimeMessage message = new MimeMessage((Session)null);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            //message.setText("This is actual message");
+            MimeBodyPart textBodyPart = new MimeBodyPart();
+            textBodyPart.setText("This is actual message body");
+            
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            writePdf(outputStream);
+            byte[] bytes = outputStream.toByteArray();
+            
+            //construct the pdf body part
+            DataSource dataSource = new ByteArrayDataSource(bytes, "application/pdf");
+            MimeBodyPart pdfBodyPart = new MimeBodyPart();
+            pdfBodyPart.setDataHandler(new DataHandler(dataSource));
+            pdfBodyPart.setFileName("test.pdf");
+            
+            MimeMultipart mimeMultipart = new MimeMultipart();
+            mimeMultipart.addBodyPart(textBodyPart);
+            mimeMultipart.addBodyPart(pdfBodyPart);
+            
+            message.setContent(mimeMultipart);
+			/*
+			 * final byte[] attachment = "i am a boy".getBytes(); MimeMultipart multipart =
+			 * new MimeMultipart(); MimeBodyPart part = new MimeBodyPart();
+			 * part.setDataHandler( new DataHandler( new DataSource() {
+			 * 
+			 * public String getContentType() { return "application/pdf"; }
+			 * 
+			 * public InputStream getInputStream() throws IOException { return new
+			 * ByteArrayInputStream(attachment); }
+			 * 
+			 * public String getName() { return "myfile.pdf"; }
+			 * 
+			 * public OutputStream getOutputStream() throws IOException { return null; }}));
+			 * part.setFileName("myfile.pdf"); multipart.addBodyPart(part);
+			 * message.setContent(multipart);
+			 * 
+			 * message.writeTo(System.out);
+			 */
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (Exception mex) {
+            mex.printStackTrace();
+        }
+
+    }
+	private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
+            Font.BOLD);
+    private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+            Font.NORMAL, BaseColor.RED);
+    private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
+            Font.BOLD);
+    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+            Font.BOLD);
+    
+    private static void addEmptyLine(Paragraph paragraph, int number) {
+        for (int i = 0; i < number; i++) {
+            paragraph.add(new Paragraph(" "));
+        }
+    }
+    
+    private static void createList(Section subCatPart) {
+        List list = new List(true, false, 10);
+        list.add(new ListItem("First point"));
+        list.add(new ListItem("Second point"));
+        list.add(new ListItem("Third point"));
+        subCatPart.add(list);
+    }
+    
+    private static void createTable(Section subCatPart)
+            throws BadElementException {
+        PdfPTable table = new PdfPTable(3);
+
+        // t.setBorderColor(BaseColor.GRAY);
+        // t.setPadding(4);
+        // t.setSpacing(4);
+        // t.setBorderWidth(1);
+
+        PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
+        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(c1);
+
+        c1 = new PdfPCell(new Phrase("Table Header 2"));
+        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(c1);
+
+        c1 = new PdfPCell(new Phrase("Table Header 3"));
+        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(c1);
+        table.setHeaderRows(1);
+
+        table.addCell("1.0");
+        table.addCell("1.1");
+        table.addCell("1.2");
+        table.addCell("2.1");
+        table.addCell("2.2");
+        table.addCell("2.3");
+
+        subCatPart.add(table);
+
+    }
+    
+    private static void addTitlePage(Document document)
+            throws DocumentException {
+        Paragraph preface = new Paragraph();
+        // We add one empty line
+        addEmptyLine(preface, 1);
+        // Lets write a big header
+        preface.add(new Paragraph("Title of the document", catFont));
+
+        addEmptyLine(preface, 1);
+        // Will create: Report generated by: _name, _date
+        preface.add(new Paragraph(
+                "Report generated by: " + System.getProperty("user.name") + ", " + new Date(), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                smallBold));
+        addEmptyLine(preface, 3);
+        
+        PdfPTable table = new PdfPTable(3); // 3 columns.
+        table.setWidthPercentage(100); //Width 100%
+        table.setSpacingBefore(10f); //Space before table
+        table.setSpacingAfter(10f); //Space after table
+ 
+        //Set Column widths
+        float[] columnWidths = {1f, 1f, 1f};
+        table.setWidths(columnWidths);
+ 
+        PdfPCell cell1 = new PdfPCell(new Paragraph("Column 1"));
+        cell1.setBorderColor(BaseColor.BLUE);
+        cell1.setPaddingLeft(10);
+        cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        PdfPCell cell2 = new PdfPCell(new Paragraph("Column 2"));
+        cell2.setBorderColor(BaseColor.GREEN);
+        cell2.setPaddingLeft(10);
+        cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        PdfPCell cell3 = new PdfPCell(new Paragraph("Column 3"));
+        cell3.setBorderColor(BaseColor.RED);
+        cell3.setPaddingLeft(10);
+        cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell3.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        //To avoid having the cell border and the content overlap, if you are having thick cell borders
+        //cell1.setUserBorderPadding(true);
+        //cell2.setUserBorderPadding(true);
+        //cell3.setUserBorderPadding(true);
+ 
+        table.addCell(cell1);
+        table.addCell(cell2);
+        table.addCell(cell3);
+        
+        preface.add(table);
+
+        addEmptyLine(preface, 8);
+
+        preface.add(new Paragraph(
+                "This document is a preliminary version and not subject to your license agreement or any other agreement with vogella.com ;-).",
+                redFont));
+
+        document.add(preface);
+        // Start a new page
+        document.newPage();
+    }
+	/**
+     * Writes the content of a PDF file (using iText API)
+     * to the {@link OutputStream}.
+     * @param outputStream {@link OutputStream}.
+     * @throws Exception
+     */
+    public static void writePdf(OutputStream outputStream) throws Exception {
+    	
+    	
+        
+        Document document = new Document();
+        PdfWriter.getInstance(document, outputStream);
+         
+        document.open();
+         
+        document.addTitle("Booking Report PDF");
+        document.addSubject("Booking Report PDF");
+        document.addKeywords("jumeirah, email");
+        document.addAuthor("Mani Vincent");
+        document.addCreator("Mani Vincent");
+         
+        Paragraph paragraph = new Paragraph();
+        // We add one empty line
+        addEmptyLine(paragraph, 1);
+        
+        paragraph.add(new Paragraph("Jumeirah Booking Report", catFont));
+        
+        addEmptyLine(paragraph, 3);
+        
+        PdfPTable table = new PdfPTable(3); // 3 columns.
+        table.setWidthPercentage(100); //Width 100%
+        table.setSpacingBefore(10f); //Space before table
+        table.setSpacingAfter(10f); //Space after table
+ 
+        //Set Column widths
+        float[] columnWidths = {1f, 1f, 1f};
+        table.setWidths(columnWidths);
+ 
+        PdfPCell cell1 = new PdfPCell(new Paragraph("Column 1"));
+        cell1.setBorderColor(BaseColor.BLUE);
+        cell1.setPaddingLeft(10);
+        cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        PdfPCell cell2 = new PdfPCell(new Paragraph("Column 2"));
+        cell2.setBorderColor(BaseColor.GREEN);
+        cell2.setPaddingLeft(10);
+        cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        PdfPCell cell3 = new PdfPCell(new Paragraph("Column 3"));
+        cell3.setBorderColor(BaseColor.RED);
+        cell3.setPaddingLeft(10);
+        cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell3.setVerticalAlignment(Element.ALIGN_MIDDLE);
+ 
+        //To avoid having the cell border and the content overlap, if you are having thick cell borders
+        //cell1.setUserBorderPadding(true);
+        //cell2.setUserBorderPadding(true);
+        //cell3.setUserBorderPadding(true);
+ 
+        table.addCell(cell1);
+        table.addCell(cell2);
+        table.addCell(cell3);
+        
+        paragraph.add(table);
+        
+        addEmptyLine(paragraph, 1);
+        
+        paragraph.add(new Paragraph(
+                "Report generated by: Mani Vincent, " + new Date(), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                smallBold));
+        addEmptyLine(paragraph, 3);
+        document.add(paragraph);
+         
+        document.close();
+    }
+
+}
